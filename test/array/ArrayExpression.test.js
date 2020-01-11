70f8a37144227bf4d44cf6d47e119b0ea4a15342
bplok20010 <nobo.zhou@foxmail.com>
2020-01-11 23:00:34 +0800

    Add ArrayExpression.test.js

---
+++ test/array/ArrayExpression.test.js
@@ -0,0 +1,64 @@
+const { evaluate } = require("../../lib");
+
+test("ArrayExpression-1", () => {
+	const arr = evaluate(
+		`
+const arr = [1, 2, 3];
+arr.push(4);
+arr;
+  `
+	);
+
+	expect(Array.isArray(arr)).toBe(true);
+	expect(arr.length).toBe(4);
+	expect(arr).toEqual([1, 2, 3, 4]);
+});
+
+test("ArrayExpression-2", () => {
+	const arr = evaluate(
+		`
+const arr = [,,1,2,3,,,];
+arr.push(4);
+arr;
+  `
+	);
+
+	expect(Array.isArray(arr)).toBe(true);
+	expect(arr.length).toBe(8);
+});
+
+test("ArrayExpression-3", () => {
+	const arr = evaluate(
+		`
+function _t(){
+    return 1 
+}
+var a = 1;
+const arr = [a++, _t() + 2, 3  + 3, undefined];
+arr.push(4);
+arr;
+  `
+	);
+
+	expect(Array.isArray(arr)).toBe(true);
+	expect(arr.length).toBe(5);
+	expect(arr).toEqual([1, 3, 6, undefined, 4]);
+});
+
+test("ArrayExpression-4", () => {
+	const arr = evaluate(
+		`
+function _t(){
+    return 1 
+}
+var a = 1;
+const arr = [1,2,null,undefined];
+arr.push(4);
+arr;
+  `
+	);
+
+	expect(Array.isArray(arr)).toBe(true);
+	expect(arr.length).toBe(5);
+	expect(arr).toEqual([1, 2, null, undefined, 4]);
+});
