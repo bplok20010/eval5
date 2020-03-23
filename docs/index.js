ab84cbfeda0c5cdeb4cffa79b81b9da22884c970
bplok20010 <nobo.zhou@foxmail.com>
2020-03-24 07:41:52 +0800

    Add echarts example

---
+++ docs/index.js
@@ -0,0 +1,14 @@
+version.innerHTML = "version: " + eval5.Interpreter.version;
+var interpreter = new eval5.Interpreter(window);
+function run() {
+	var source = code.value;
+
+	try {
+		var result = interpreter.evaluate(source);
+		results.innerHTML = result;
+		console.log(result);
+	} catch (e) {
+		console.log(e);
+		results.innerHTML = `<div class="error">${e.message}</div>`;
+	}
+}
